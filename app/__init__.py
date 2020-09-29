1ed01e8ffba166ddd63eb7ff6a6c8df8491a3648
leondante-ctrl <maxgamerdu@gmail.com>
2020-09-29 08:38:20 +0300

    debugging

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_uploads import configure_uploads, IMAGES
+from flask_uploads import configure_uploads, IMAGES, UploadSet
 
 # creating instances of flask extensions by initializing the flask extenstions
 db = SQLAlchemy()
